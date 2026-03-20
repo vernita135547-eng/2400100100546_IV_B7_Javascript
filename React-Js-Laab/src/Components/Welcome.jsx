10f6a5fb1d46cd956aa1b64d1298d63c670d64f7
vernita135547-eng <vernita135547@gmail.com>
2026-03-20 15:20:32 +0530

    message

--- React-Js-Laab/src/Components/Welcome.jsx
+++ React-Js-Laab/src/Components/Welcome.jsx
@@ -1,11 +1,11 @@
 //function Welcome(props){
-function Welcome({name}){
+function Welcome({name = '', greet = ''}){
     return(
         <div>
             <h2>Welcome Component</h2>
             {/*<h3>{props.name}</h3>*/}
             <h3>{name}</h3>
-            <h3>{props.greet}</h3>
+            <h3>{greet}</h3>
             <p>This is our first reusable component</p>
         </div>
     )
